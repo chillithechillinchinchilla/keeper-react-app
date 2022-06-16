6f9bd95650485ff3e84f0f92e11200eb6d21d98a
Michael Grimsley <24793858+chillithechillinchinchilla@users.noreply.github.com>
2022-06-16 12:21:02 -0400

    Convert to arrow function for better legibility, but I think that's a lie.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,26 +10,16 @@ import notes from "../notes";
 //set format in NOte.jsx
 //set map in Note funciton
 
-//Generic map function to create a single note/entry, called from the array.map in App(). It the creates an individual Note.jsx, 
-//but allows map to iterate for each array object.
-//<Note title={"tuff"} content={"morestuff"}/>
-function createNote(note){
-    return (
-        <Note 
-        key={note.key}
-        title={note.title}
-        content={note.content}
-        />
-    );
-}
-
-
 function App() {
     return (
         <div>
             <Header />
-            <dl>{notes.map(createNote)}</dl>
-
+            {notes.map( note=> (
+                <Note key={note.key} 
+                title={note.title} 
+                content={note.content}
+                />
+            ))}
             <Footer />
         </div>
     );
